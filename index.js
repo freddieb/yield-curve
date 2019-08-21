e21cff8cb0088331ecc7ac4d7a781829d2b0d02b
freddieb <freddieiabrown@hotmail.co.uk>
2019-08-21 12:59:47 +0100

    Initial server structure

---
+++ index.js
@@ -0,0 +1,26 @@
+const dotenv = require('dotenv')
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+
+// Load the configuration into process.env.<value> variables
+dotenv.config()
+
+// Initialise the server
+var app = express()
+
+// Route to send chart data
+app.use('/', require('./routes'))
+
+// Setup templating with handlebars
+app.use(express.static(path.join(__dirname, '/public')))
+hbs.registerPartials(path.join(__dirname, '/views/partials'))
+app.set('view engine', 'hbs')
+
+// Set app port
+app.set('port', process.env.PORT)
+
+// Start the server, with it listening on the preset port
+var server = app.listen(app.get('port'), () => {
+  console.log('Foresight server listening on port: ' + server.address().port)
+})
---
+++ routes/yieldData.js
@@ -0,0 +1,5 @@
+module.exports = (req, res) => {
+  return res.status(200).json({
+    'temp': 'data'
+  })
+}
